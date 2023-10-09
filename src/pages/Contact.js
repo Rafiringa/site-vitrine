bd2ef65981311cc09ab8c59a015cb9cdc2c1e347
Ampy <rafiringa.a@gmail.com>
2023-10-09 19:08:12 +0300

    Realisation site vitrine avec React

---
+++ src/pages/Contact.js
@@ -0,0 +1,84 @@
+import React from "react";
+import ContactForm from "../components/ContactForm";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import SocialNetwork from "../components/SocialNetwork";
+import Buttons from "../components/Buttons";
+import { motion } from "framer-motion";
+
+const Contact = () => {
+  const pageTransition = {
+    in: {
+      opacity: 1,
+      x: 0,
+    },
+    out: {
+      opacity: 0,
+      x: 300,
+    },
+  };
+
+  return (
+    <main>
+      <Mouse />
+      <motion.div
+        initial="out"
+        exit="out"
+        animate="in"
+        variants={pageTransition}
+        transition={{ duration: 0.5 }}
+        className="contact"
+      >
+        <Navigation />
+        <Logo />
+        <ContactForm />
+        <div className="contact-infos">
+          <div className="address">
+            <div className="content">
+              <h4>adresse</h4>
+              <p>12 rue Laplace</p>
+              <p>6400 Biarritz</p>
+            </div>
+          </div>
+          <div className="phone">
+            <div className="content">
+              <h4>téléphone</h4>
+              <CopyToClipboard text="0344485278" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Téléphone copié !")}
+                >
+                  0344485278
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <div className="email">
+            <div className="content">
+              <h4>email</h4>
+              <CopyToClipboard text="rfagency@gmail.com" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Ëmail copié !")}
+                >
+                  rfagency@gmail.com
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <SocialNetwork />
+          <div className="credits">
+            <p>Rafiringa - 2023</p>
+          </div>
+        </div>
+        <Buttons left={"/projet-4"} />
+      </motion.div>
+    </main>
+  );
+};
+
+export default Contact;
